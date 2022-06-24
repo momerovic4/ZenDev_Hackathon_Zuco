123d134ea4cecbed72f6144745d711287c083343
Haris Kukuruzović <haris_kukuruzovic@hotmail.com>
2022-06-24 19:26:37 +0200

    dependecy inject

---
+++ ZucoBiH/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZucoBiH
+
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddBackendServices(this IServiceCollection services)
+        {
+            services.AddDbContext<Context>(options =>
+            {
+           
+                var dbConnString = @"";
+                options.UseSqlServer(dbConnString,
+                    dboContextOptions => dboContextOptions.EnableRetryOnFailure(2));
+            });
+
+            return services;
+        }
+    }
+}
