51b221c1a4cf0f0d31ab7b016c92f24178444217
Haris Kukuruzović <haris_kukuruzovic@hotmail.com>
2022-06-25 10:27:08 +0200

    pages api

--- ZucoBiH/Models/Post.cs
+++ ZucoBiH/Models/Post.cs
@@ -7,11 +7,11 @@ namespace ZucoBiH.Models
         public string Title { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Description { get; set; }
-        public double Logngitude { get; set; }
+        public double Longitude { get; set; }
         public double Latitude { get; set; }
         public string Category { get; set; }
         public string Image64 { get; set; }
-        public bool Done { get; set; }
+        public bool Approved { get; set; }
         public bool Positive { get; set; }
     }
 }
